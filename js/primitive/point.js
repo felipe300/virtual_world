7ca3ed3b5c9700ab3f2ada47914e7c3bd62357a9
Felipe300 <caracoloro@gmail.com>
2023-12-18 17:36:38 -0300

    feat: add linting

--- js/primitive/point.js
+++ js/primitive/point.js
@@ -1,40 +1,44 @@
 class Point {
-	constructor (x, y) {
-		this.x = x
-		this.y = y
-	}
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	equals (point) {
-		return this.x === point.x && this.y === point.y
-	}
+  equals(point) {
+    return this.x === point.x && this.y === point.y;
+  }
 
-	#arc (radious) {
-		ctx.arc(this.x, this.y, radious, 0, Math.PI * 2)
-	}
+  #arc(radious) {
+    ctx.arc(this.x, this.y, radious, 0, Math.PI * 2);
+  }
 
-	draw (ctx, { size = 18, color = "black", outline = false, fill = false } = {}) {
-		const rad = size / 2
-		ctx.beginPath()
-		ctx.fillStyle = color
-		// ctx.arc(this.x, this.y, rad, 0, Math.PI * 2)
-		this.#arc(rad * 1)
+  draw(
+    ctx,
+    { size = 18, color = "black", outline = false, fill = false } = {},
+  ) {
+    const rad = size / 2;
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    // ctx.arc(this.x, this.y, rad, 0, Math.PI * 2)
+    this.#arc(rad * 1);
 
-		ctx.fill()
-		if (outline) {
-			ctx.beginPath()
-			ctx.lineWidth = 2
-			ctx.strokeStyle = "yellow"
-			// ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
-			this.#arc(rad * 0.6)
-			ctx.stroke()
-		}
+    ctx.fill();
+    if (outline) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      // ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
+      this.#arc(rad * 0.6);
+      ctx.stroke();
+    }
 
-		if (fill) {
-			ctx.beginPath()
-			// ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
-			this.#arc(rad * 0.4)
-			ctx.fillStyle = "yellow"
-			ctx.fill()
-		}
-	}
+    if (fill) {
+      ctx.beginPath();
+      // ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
+      this.#arc(rad * 0.4);
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+    }
+  }
 }
+
