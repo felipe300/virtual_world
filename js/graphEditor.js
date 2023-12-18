e1869468a62fc9df584fcb653454d1891ad8f915
Felipe300 <caracoloro@gmail.com>
2023-12-18 13:21:06 -0300

    feat: add graph editor

---
+++ js/graphEditor.js
@@ -0,0 +1,91 @@
+class GraphEditor {
+	constructor (canvas, graph) {
+		this.canvas = canvas
+		this.graph = graph
+
+		this.ctx = this.canvas.getContext("2d")
+
+		this.selected = null
+		this.hovered = null
+		this.dragging = false
+		this.mouse = null
+
+		this.#addEventListeners()
+	}
+
+	#addEventListeners () {
+		this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this))
+		this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this))
+		// remove right click menu, when right click
+		this.canvas.addEventListener("contextmenu", this.#handleContextMenu.bind(this))
+		this.canvas.addEventListener("mouseup", this.#handleMouseMoveUp.bind(this))
+	}
+
+	#select (point) {
+		if (this.selected) {
+			this.graph.tryAddSegment(new Segment(this.selected, point))
+		}
+		this.selected = point
+	}
+
+	#handleMouseDown (evt) {
+		// right click
+		if (evt.button === 2) {
+			if (this.selected) {
+				this.selected = null
+			} else if (this.hovered) {
+				this.#removePoint(this.hovered)
+			}
+		}
+
+		// left click
+		if (evt.button === 0) {
+			if (this.hovered) {
+				this.#select(this.hovered)
+				this.dragging = true
+				return
+			}
+			this.graph.addPoint(this.mouse)
+			this.#select(this.mouse)
+			this.hovered = this.mouse
+		}
+	}
+
+	#handleMouseMove (evt) {
+		this.mouse = new Point(evt.offsetX, evt.offsetY)
+		this.hovered = getNearestPoint(this.mouse, this.graph.points, 10)
+		if (this.dragging) {
+			this.selected.x = this.mouse.x
+			this.selected.y = this.mouse.y
+		}
+	}
+
+	#handleContextMenu (evt) {
+		evt.preventDefault()
+	}
+
+	#handleMouseMoveUp (evt) {
+		this.dragging = false
+	}
+
+	#removePoint (point) {
+		this.graph.removePoint(point)
+		this.hovered = null
+		if (this.selected === point) {
+			this.selected = null
+		}
+	}
+
+	display () {
+		this.graph.draw(this.ctx)
+		if (this.hovered) {
+			this.hovered.draw(this.ctx, { fill: true })
+		}
+
+		if (this.selected) {
+			const intent = this.hovered ? this.hovered : this.mouse
+			new Segment(this.selected, intent).draw(ctx, { dash: [3, 3] })
+			this.selected.draw(this.ctx, { outline: true })
+		}
+	}
+}
--- js/primitive/point.js
+++ js/primitive/point.js
@@ -4,19 +4,37 @@ class Point {
 		this.y = y
 	}
 
-	// check if random point does exits
 	equals (point) {
 		return this.x === point.x && this.y === point.y
 	}
 
-	// size in pixels
-	draw (ctx, size = 18, color = "black") {
+	#arc (radious) {
+		ctx.arc(this.x, this.y, radious, 0, Math.PI * 2)
+	}
+
+	draw (ctx, { size = 18, color = "black", outline = false, fill = false } = {}) {
 		const rad = size / 2
 		ctx.beginPath()
 		ctx.fillStyle = color
-		// initial points, radious, full cirle (0 deg, cirlce PI)
-		// Math.PI * 2 = 360°
-		ctx.arc(this.x, this.y, rad, 0, Math.PI * 2)
+		// ctx.arc(this.x, this.y, rad, 0, Math.PI * 2)
+		this.#arc(rad * 1)
+
 		ctx.fill()
+		if (outline) {
+			ctx.beginPath()
+			ctx.lineWidth = 2
+			ctx.strokeStyle = "yellow"
+			// ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
+			this.#arc(rad * 0.6)
+			ctx.stroke()
+		}
+
+		if (fill) {
+			ctx.beginPath()
+			// ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2)
+			this.#arc(rad * 0.4)
+			ctx.fillStyle = "yellow"
+			ctx.fill()
+		}
 	}
 }
