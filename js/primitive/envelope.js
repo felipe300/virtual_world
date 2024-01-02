d1ed4ed62b783f3be958b93c665bfa0da4134865
Felipe300 <caracoloro@gmail.com>
2024-01-02 13:20:09 -0300

    feat: save maps, save single world with markings

--- js/primitive/envelope.js
+++ js/primitive/envelope.js
@@ -1,7 +1,16 @@
 class Envelope {
   constructor(skeleton, width, roundess = 1) {
-    this.skeleton = skeleton;
-    this.poly = this.#generatePolygon(width, roundess);
+    if (skeleton) {
+      this.skeleton = skeleton;
+      this.poly = this.#generatePolygon(width, roundess);
+    }
+  }
+
+  static load(info) {
+    const env = new Envelope();
+    env.skeleton = new Segment(info.skeleton.p1, info.skeleton.p2);
+    env.poly = Polygon.load(info.poly);
+    return env;
   }
 
   #generatePolygon(width, roundess) {
